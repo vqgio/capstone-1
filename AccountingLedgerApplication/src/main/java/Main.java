7b0beb0f1963c1a30302b2550a854133a37308ee
Gio <giovannizavala82@gmail.com>
2025-04-28 11:25:58 -0500

    Created file path date and time and created a home screen display

--- AccountingLedgerApplication/src/main/java/Main.java
+++ AccountingLedgerApplication/src/main/java/Main.java
@@ -1,3 +1,33 @@
-public class Main {
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
+public class Main {
+    private static final String filePath = "C:/Users/rsant/pluralsight/capstone-1/AccountingLedgerApplication/src/main/resources/transactions.csv";
+    private static final DateTimeFormatter yearMonthFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter hourMinuteFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean(running) = true;
+        while(running) {
+            displayHomeScreen();
+            String choice = scanner.nextLine().toUpperCase();
+            switch (choice) {
+                case "D":
+                    addDeposit();
+                    break;
+                case "P":
+                    makePayment();
+                    break;
+                case "L":
+                    displayLedger();
+                case "X":
+                    running = false;
+                    System.out.println("Logging out...Have a nice day!");
+                    break;
+                default:
+                    System.out.println("Sorry that is not an option! Please try again. ")
+            }
+        }
+    }
 }
