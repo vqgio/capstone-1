c44e52c3c03ebaf73d63f8fdab52340e961dae1f
Gio <giovannizavala82@gmail.com>
2025-07-25 13:08:46 -0500

    moved main to model package.

---
+++ AccountingLedgerApplication/src/main/java/model/Main.java
@@ -0,0 +1,12 @@
+package model;
+
+import ui.ConsoleUI;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleUI ui = new ConsoleUI();
+        ui.display();
+    }
+}
+
+
